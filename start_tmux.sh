4982e2b3ebe4599cdee524e9bf117df3797f7d32
Andrew Kraemer <andrew@cerebri.com>
2018-10-09 17:53:33 +0000

    created script to kick off prefered tmux setup

---
+++ start_tmux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh 
+
+# check if session exists
+tmux has-session -t dev
+if [ $? != 0 ]
+then
+tmux new-session -d -s dev -n vvim
+
+tmux selectp -t 1 # name pane 1
+tmux send-keys "vvim" C-m
+
+sleep 1 # give neovim enough time to fill screen
+
+# create upper right pane w/ python 3
+tmux splitw -h -p 20
+tmux send-keys "py3" C-m
+
+# create lower right pane w/ python 3 in scratch folder
+tmux splitw -v
+tmux send-keys "py3;cd /scratch" C-m
+
+# tmux selectp -t 1 # select pane 1
+fi
+
+tmux attach -t dev
